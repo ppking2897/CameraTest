273a152956aeadd56b242abc213d0061fef9ba9e
ppking <ppking2897@gmail.com>
2017-11-03 15:04:58 +0800

    2017.11.03

--- app/src/main/java/ea/cameratest/EnterPictureActivity.java
+++ app/src/main/java/ea/cameratest/EnterPictureActivity.java
@@ -1,25 +1,26 @@
 package ea.cameratest;
 
-import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 import ea.cameratest.MainActivity.MainActivity;
 
+
 public class EnterPictureActivity extends AppCompatActivity {
 
-    private CountdownTime countdownTime;
-    private MainActivity activity;
+    private CountTime countTime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_picture);
 
-        countdownTime = new CountdownTime(100000 , 1000);
+        countTime = new CountTime(5000 , 1000 , this);
+        countTime.start();
 
-        countdownTime.start();
     }
 
     public void enter(View view) {
@@ -28,9 +29,22 @@ public class EnterPictureActivity extends AppCompatActivity {
         finish();
     }
 
-    public void intentActivity(){
-        Intent it = new Intent(this , MainActivity.class);
-        startActivity(it);
-        finish();
+    public class CountTime extends CountDownTimer{
+        private Context context;
+
+        public CountTime(long millisInFuture, long countDownInterval , Context context) {
+            super(millisInFuture, countDownInterval);
+            this.context = context;
+        }
+
+        @Override
+        public void onTick(long l) {
+
+        }
+
+        @Override
+        public void onFinish() {
+            Intent it = new Intent(context , MainActivity.class);
+        }
     }
 }
